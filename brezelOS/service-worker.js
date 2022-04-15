0542da81b42c0ec43451549d410da9f266c483db
Pingui19 <gabriele.formicola06@gmail.com>
2022-04-15 13:57:59 +0200

    brezelOS addition
    java program not even made by me imma cry amogus

---
+++ brezelOS/service-worker.js
@@ -0,0 +1,41 @@
+console.debug("[ServiceWorker] Initializing");
+
+let config = {};
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        fetch("./package_deeee909c7c2a44cd3df7456d0125daf95b9f89a/uno-config.js")
+            .then(r => r.text()
+                .then(configStr => {
+                    eval(configStr);
+                    caches.open('package_deeee909c7c2a44cd3df7456d0125daf95b9f89a').then(function (cache) {
+                        console.debug('[ServiceWorker] Caching app binaries and content');
+                        return cache.addAll(config.offline_files);
+                    });
+                }
+                )
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed
